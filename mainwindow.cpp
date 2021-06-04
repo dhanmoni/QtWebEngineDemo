23cdfeae540323c3652d6490d2f36e67ef2c07c2
Dhanmoni Nath <dhanmoni@users.noreply.github.com>
2021-06-04 15:14:12 +0530

    added feature to plot graph from user input

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,12 +7,16 @@
 #include <QMessageBox>
 #include <QWebChannel>
 #include <QDebug>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qRegisterMetaType<XYValue>("XYValue");
     QWebEngineView* view = new QWebEngineView(parent);
     QWebChannel * channel = new QWebChannel(this);
     view->page()->setWebChannel(channel);
@@ -37,9 +41,61 @@ void MainWindow::jscallme(const QString &datafromjs)
 
 void MainWindow::on_pushButton_2_clicked()
 {
-    qDebug() << "button clicked!";
-    qDebug() << "emitting function" << m_data;
-    m_num = 10;
-    m_data += "added!";
-   emit dataChanged(m_data, m_num);
+
+     QJsonArray x_val;
+     QJsonArray y_val;
+
+     x_val.push_back(2);
+     x_val.push_back(4);
+     x_val.push_back(6);
+     x_val.push_back(9);
+     y_val.push_back(15);
+     y_val.push_back(4);
+     y_val.push_back(13);
+     y_val.push_back(17);
+
+     QJsonObject xy_vals;
+     xy_vals [ "x_val" ] = x_val;
+     xy_vals [ "y_val" ] = y_val;
+
+
+     QJsonObject obj;
+     obj [ "plottingData" ] = xy_vals;
+
+     //to see the JSON output
+     QJsonDocument doc ( obj );
+     qDebug() << doc.toJson ( QJsonDocument::Compact );
+
+     //emit signal
+     emit dataChanged(obj);
+}
+
+
+void MainWindow::on_AddLine_clicked()
+{
+    QJsonArray x_val;
+    QJsonArray y_val;
+
+    qDebug() << "add line button clicked!" << ui->xValues->text();
+    QStringList xData = ui->xValues->text().split(",");
+    QStringList yData = ui->yValues->text().split(",");
+
+    foreach(QString num, xData)
+    {
+        x_val.push_back(num.toInt());
+    }
+    foreach(QString num, yData)
+    {
+        y_val.push_back(num.toInt());
+    }
+
+    QJsonObject xy_vals;
+    xy_vals [ "x_val" ] = x_val;
+    xy_vals [ "y_val" ] = y_val;
+
+
+    QJsonObject obj;
+    obj [ "plottingData" ] = xy_vals;
+
+    emit dataChanged(obj);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -7,23 +7,35 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+struct XYValue
+{
+    QVector<int> xValues;
+    QVector<int> yValues;
+};
+Q_DECLARE_METATYPE(XYValue)
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    Q_PROPERTY(QString data MEMBER m_data NOTIFY dataChanged)
-    Q_PROPERTY(int num MEMBER m_num NOTIFY dataChanged)
+//    Q_PROPERTY(QString data MEMBER m_data NOTIFY dataChanged)
+//    Q_PROPERTY(int num MEMBER m_num NOTIFY dataChanged)
+//    Q_PROPERTY(QVector<int> xValues MEMBER m_xValues NOTIFY dataChanged)
+//    Q_PROPERTY(QVector<int> yValues MEMBER m_yValues NOTIFY dataChanged)
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 public slots:
     void jscallme(const QString &datafromjs);
     void on_pushButton_2_clicked();
+    void on_AddLine_clicked();
 signals:
-    void dataChanged(const QString & data,const int & num);
-
+    //void dataChanged(const QVector<int> & xValues,const QVector<int> & yValues);
+    void dataChanged(const QJsonObject & val);
 private:
     Ui::MainWindow *ui;
      QString m_data;
      int m_num;
+//     QVector<int> m_xValues;
+//     QVector<int> m_yValues;
 };
 #endif // MAINWINDOW_H
